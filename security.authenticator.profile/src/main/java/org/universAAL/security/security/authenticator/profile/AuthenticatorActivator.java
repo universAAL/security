609beaf686864bdafaaff71b128ef04ad56e6e7f
Alejandro Medrano <amedrano@lst.tfo.upm.es>
2013-11-01 16:02:23 +0000

    completed Profile Authenticator (needs testing).

--- security.authenticator.profile/src/main/java/org/universAAL/security/security/authenticator/profile/AuthenticatorActivator.java
+++ security.authenticator.profile/src/main/java/org/universAAL/security/security/authenticator/profile/AuthenticatorActivator.java
@@ -2,12 +2,15 @@ package org.universAAL.security.security.authenticator.profile;
 
 import org.universAAL.middleware.container.ModuleActivator;
 import org.universAAL.middleware.container.ModuleContext;
+import org.universAAL.middleware.container.SharedObjectListener;
 import org.universAAL.middleware.container.utils.LogUtils;
+import org.universAAL.middleware.serialization.MessageContentSerializerEx;
 
-public class AuthenticatorActivator implements ModuleActivator {
+public class AuthenticatorActivator implements ModuleActivator, SharedObjectListener {
 
-    static ModuleContext context;
+    ModuleContext context;
     private UserPasswordCallee callee;
+    private MessageContentSerializerEx serializer; 
 
     /** {@ inheritDoc} */
     public void start(ModuleContext mc) throws Exception {
@@ -16,8 +19,15 @@ public class AuthenticatorActivator implements ModuleActivator {
 	/*
 	 * uAAL stuff
 	 */
+	Object[] obj = context.getContainer()
+		.fetchSharedObject(context, 
+			new Object[] { MessageContentSerializerEx.class.getName() }, this);
+	if (obj.length > 0){
+	    sharedObjectAdded(obj[0], null);
+	}
+	
 	UserPasswordProfileService.initialize(context);
-	callee = new UserPasswordCallee(context);
+	callee = new UserPasswordCallee(this);
 	LogUtils.logDebug(context, getClass(), "start", "Started.");
     }
 
@@ -25,4 +35,27 @@ public class AuthenticatorActivator implements ModuleActivator {
     public void stop(ModuleContext mc) throws Exception {
 	callee.close();
     }
+
+    /** {@ inheritDoc}	 */
+    public synchronized void sharedObjectAdded(Object sharedObj, Object removeHook) {
+	serializer = (MessageContentSerializerEx) sharedObj;
+	this.notifyAll();
+    }
+
+    /** {@ inheritDoc}	 */
+    public synchronized void sharedObjectRemoved(Object removeHook) {
+	if (removeHook.equals(serializer)){
+	    serializer = null;
+	}
+    }
+    
+    public synchronized MessageContentSerializerEx getSerializer(){
+	while (serializer == null){
+	    try {
+		this.wait();
+	    } catch (InterruptedException e) {}
+	}
+	return serializer; 
+    }
+
 }
--- security.authenticator.profile/src/main/java/org/universAAL/security/security/authenticator/profile/UserPasswordCallee.java
+++ security.authenticator.profile/src/main/java/org/universAAL/security/security/authenticator/profile/UserPasswordCallee.java
@@ -17,17 +17,27 @@
 
 package org.universAAL.security.security.authenticator.profile;
 
+import java.io.InputStream;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.util.List;
+import java.util.Scanner;
 
 import org.universAAL.middleware.container.ModuleContext;
+import org.universAAL.middleware.container.utils.LogUtils;
+import org.universAAL.middleware.owl.MergedRestriction;
+import org.universAAL.middleware.rdf.PropertyPath;
 import org.universAAL.middleware.service.CallStatus;
+import org.universAAL.middleware.service.DefaultServiceCaller;
 import org.universAAL.middleware.service.ServiceCall;
 import org.universAAL.middleware.service.ServiceCallee;
+import org.universAAL.middleware.service.ServiceRequest;
 import org.universAAL.middleware.service.ServiceResponse;
 import org.universAAL.middleware.service.owls.process.ProcessOutput;
 import org.universAAL.middleware.service.owls.profile.ServiceProfile;
+import org.universAAL.ontology.che.ContextHistoryService;
 import org.universAAL.ontology.profile.User;
+import org.universAAL.ontology.security.SecurityOntology;
 import org.universAAL.ontology.security.UserPasswordCredentials;
 
 /**
@@ -36,6 +46,10 @@ import org.universAAL.ontology.security.UserPasswordCredentials;
  */
 public class UserPasswordCallee extends ServiceCallee {
 
+    private static final String UTF_8 = "utf-8";
+    private static final String OUTPUT_RESULT_STRING = SecurityOntology.NAMESPACE
+	    + "outputfromCHE";
+    private AuthenticatorActivator auth;
     /**
      * @param context
      * @param realizedServices
@@ -48,8 +62,9 @@ public class UserPasswordCallee extends ServiceCallee {
     /**
      * 
      */
-    public UserPasswordCallee(ModuleContext mc) {
-	this(mc,UserPasswordProfileService.profs);
+    public UserPasswordCallee(AuthenticatorActivator aa) {
+	this(aa.context,UserPasswordProfileService.profs);
+	auth = aa;
     }
 
     /** {@ inheritDoc}	 */
@@ -67,13 +82,17 @@ public class UserPasswordCallee extends ServiceCallee {
 	
 	String cmd = call.getProcessURI();
 	if (cmd.equals(UserPasswordProfileService.AUTHENTICATE_USR_PASSWORD_SERVICE)){
-	    UserPasswordCredentials upc = (UserPasswordCredentials) call.getInputValue(UserPasswordProfileService.CRED_IN);
-	    User u = authenticate(upc.getUsername(),upc.getPassword(), upc.getDigestAlgorithm());
-	    if (u != null){
+	    try {
+		UserPasswordCredentials upc = (UserPasswordCredentials) call.getInputValue(UserPasswordProfileService.CRED_IN);
+		User u = authenticate(upc.getUsername(),upc.getPassword(), upc.getDigestAlgorithm());
+		if (u != null){
 		ProcessOutput out = new ProcessOutput(UserPasswordProfileService.USER_OUT, u);
 		ServiceResponse sr = new ServiceResponse(CallStatus.succeeded);
 		sr.addOutput(out);
 		return sr;
+		}
+	    } catch (Exception e) {
+		LogUtils.logDebug(owner, getClass(), "handleCall", new String[]{"Unexpected error"}, e);		
 	    }
 	}
 
@@ -88,14 +107,59 @@ public class UserPasswordCallee extends ServiceCallee {
      */
     private User authenticate(String username, String password,
 	    String digestAlgorithm) {
-	// TODO Auto-generated method stub
-	try {
-	    MessageDigest.getInstance(digestAlgorithm);
-	} catch (NoSuchAlgorithmException e) {
-	    // TODO Auto-generated catch block
-	    e.printStackTrace();
+	if (digestAlgorithm == null || digestAlgorithm.isEmpty()){
+	    digestAlgorithm = (String) query("GetDigestQuery", new String[]{username});
+	    try {
+		MessageDigest dig = MessageDigest.getInstance(digestAlgorithm);
+		password = new String (dig.digest(password.getBytes()));
+	    } catch (NoSuchAlgorithmException e) {
+		LogUtils.logWarn(owner, getClass(), "authenticate", new String[]{"unable to digest Password"}, e);
+	    }
+	}
+	return (User) query("GetUserQuery", new String[]{username,password,digestAlgorithm});
+    }
+    
+    private Object query(String queryFile, String[] params){
+	String q = getQuery(queryFile, params);
+	ServiceRequest getQuery = new ServiceRequest(
+		new ContextHistoryService(null), null);
+
+	MergedRestriction r = MergedRestriction.getFixedValueRestriction(
+		ContextHistoryService.PROP_PROCESSES, q);
+
+	getQuery.getRequestedService().addInstanceLevelRestriction(r,
+		new String[] { ContextHistoryService.PROP_PROCESSES });
+	getQuery.addSimpleOutputBinding(
+		new ProcessOutput(OUTPUT_RESULT_STRING), new PropertyPath(null,
+			true,
+			new String[] { ContextHistoryService.PROP_RETURNS })
+			.getThePath());
+	ServiceResponse sr = new DefaultServiceCaller(owner).call(getQuery);
+	List res = sr.getOutput(OUTPUT_RESULT_STRING, true);
+	if (res.size() >0 && res.get(0) instanceof String){
+	    return auth.getSerializer().deserialize((String) res.get(0));
 	}
 	return null;
     }
 
+    public static String getQuery(String queryFile, String[] params){
+	String query = "";
+	try {
+	    	InputStream file = UserPasswordCallee.class.getClassLoader().getResourceAsStream(queryFile);
+		query = new Scanner(file,UTF_8).useDelimiter("\\Z").next();
+		file.close();
+	} catch (Exception e){
+		/*
+		 *  either:
+		 *  	- empty file
+		 *  	- non existent file
+		 *  	- Scanner failture...
+		 *  Nothing to do here
+		 */
+	}
+	for (int i = 0; i < params.length; i++) {
+	   query = query.replace("$" + Integer.toString(i+1), params[i]);
+	}
+	return query;
+    }
 }
