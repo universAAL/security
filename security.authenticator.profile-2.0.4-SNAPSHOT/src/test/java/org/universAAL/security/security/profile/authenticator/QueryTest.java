db9648624ad2ae691533cf6c36485e2948cb2cc7
Alejandro Medrano <amedrano@lst.tfo.upm.es>
2014-01-20 15:57:27 +0000

    Automatic tag of security.authenticator.profile version: 2.0.4-SNAPSHOT

---
+++ security.authenticator.profile-2.0.4-SNAPSHOT/src/test/java/org/universAAL/security/security/profile/authenticator/QueryTest.java
@@ -0,0 +1,29 @@
+package org.universAAL.security.security.profile.authenticator;
+
+import org.universAAL.security.security.authenticator.profile.UserPasswordCallee;
+
+import junit.framework.TestCase;
+
+/**
+ * Unit test for simple App.
+ */
+public class QueryTest 
+    extends TestCase
+{
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testGetUserQuery() {
+	String s = UserPasswordCallee.getQuery("GetUserQuery", new String []{"usr1","password", "secret"});
+	assertNotNull(s);
+    }
+    
+    /**
+     * Rigourous Test :-)
+     */
+    public void testGetDigestQuery() {
+	String s = UserPasswordCallee.getQuery("GetDigestQuery", new String []{"usr1","password", "secret"});
+	assertNotNull(s);
+    }
+}
