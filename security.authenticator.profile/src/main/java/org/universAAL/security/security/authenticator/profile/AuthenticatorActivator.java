de05dcbeec34c8ae8789caeb6db3496e0ef49641
Alejandro Medrano <amedrano@lst.tfo.upm.es>
2014-01-20 16:00:43 +0000

    Automatic tag of security.authenticator.profile version: 3.0.0

---
+++ security.authenticator.profile/src/main/java/org/universAAL/security/security/authenticator/profile/AuthenticatorActivator.java
@@ -0,0 +1,79 @@
+/*******************************************************************************
+ * Copyright 2013 Universidad Politécnica de Madrid
+ * Copyright 2013 Fraunhofer-Gesellschaft - Institute for Computer Graphics Research
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ******************************************************************************/
+package org.universAAL.security.security.authenticator.profile;
+
+import org.universAAL.middleware.container.ModuleActivator;
+import org.universAAL.middleware.container.ModuleContext;
+import org.universAAL.middleware.container.SharedObjectListener;
+import org.universAAL.middleware.container.utils.LogUtils;
+import org.universAAL.middleware.serialization.MessageContentSerializer;
+
+public class AuthenticatorActivator implements ModuleActivator, SharedObjectListener {
+
+    ModuleContext context;
+    private UserPasswordCallee callee;
+    private MessageContentSerializer serializer; 
+
+    /** {@ inheritDoc} */
+    public void start(ModuleContext mc) throws Exception {
+	context = mc;
+	LogUtils.logDebug(context, getClass(), "start", "Starting.");
+	/*
+	 * uAAL stuff
+	 */
+	Object[] obj = context.getContainer()
+		.fetchSharedObject(context, 
+			new Object[] { MessageContentSerializer.class.getName() }, this);
+	if (obj.length > 0){
+	    sharedObjectAdded(obj[0], null);
+	}
+	
+	UserPasswordProfileService.initialize(context);
+	callee = new UserPasswordCallee(this);
+	LogUtils.logDebug(context, getClass(), "start", "Started.");
+    }
+
+    /** {@ inheritDoc} */
+    public void stop(ModuleContext mc) throws Exception {
+	callee.close();
+    }
+
+    /** {@ inheritDoc}	 */
+    public synchronized void sharedObjectAdded(Object sharedObj, Object removeHook) {
+	if (sharedObj instanceof MessageContentSerializer) {
+	    serializer = (MessageContentSerializer) sharedObj;
+	    this.notifyAll();
+	}
+    }
+
+    /** {@ inheritDoc}	 */
+    public synchronized void sharedObjectRemoved(Object removeHook) {
+	if (removeHook.equals(serializer)){
+	    serializer = null;
+	}
+    }
+    
+    public synchronized MessageContentSerializer getSerializer(){
+	while (serializer == null){
+	    try {
+		this.wait();
+	    } catch (InterruptedException e) {}
+	}
+	return serializer; 
+    }
+
+}
