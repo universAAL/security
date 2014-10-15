843bd9885ecbf69d432f5b3fcfc8bd5059ebde58
Alejandro Medrano <amedrano@lst.tfo.upm.es>
2014-10-15 14:18:12 +0000

    Automatic tag of security.session.manager version: 3.2.0

---
+++ security.session.manager/src/main/java/org/universAAL/security/session/manager/ManagerActivator.java
@@ -0,0 +1,96 @@
+/*******************************************************************************
+ * Copyright 2013 Universidad Politécnica de Madrid
+ * Copyright 2013 Fraunhofer-Gesellschaft - Institute for Computer Graphics Research
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ******************************************************************************/
+package org.universAAL.security.session.manager;
+
+import org.universAAL.middleware.container.ModuleActivator;
+import org.universAAL.middleware.container.ModuleContext;
+import org.universAAL.middleware.container.utils.LogUtils;
+import org.universAAL.security.session.manager.context.SessionPublisher;
+import org.universAAL.security.session.manager.context.SituationMonitor;
+import org.universAAL.security.session.manager.context.Subscriber;
+import org.universAAL.security.session.manager.impl.SessionManagerImpl;
+import org.universAAL.security.session.manager.service.SCallee;
+import org.universAAL.security.session.manager.service.SessionManagerService;
+
+public class ManagerActivator implements ModuleActivator {
+
+	ModuleContext context;
+	
+	SessionManager sessionManager;
+
+	private Subscriber subscriber;
+
+	private SCallee scallee;
+
+	private SessionPublisher publisher;
+
+	private SituationMonitor monitor;
+	
+	public void start(ModuleContext ctxt) throws Exception {	
+		context = ctxt;
+		LogUtils.logDebug(context, getClass(), "start", "Starting.");
+		/*
+		 * uAAL stuff
+		 */
+		LogUtils.logDebug(context, getClass(), "start", "Starting Publisher.");
+		publisher = new SessionPublisher(context);
+
+		LogUtils.logDebug(context, getClass(), "start", "Starting Situation Monitor.");
+		//monitor ;
+		
+		//Initialize sessionManager.
+		LogUtils.logDebug(context, getClass(), "start", "Starting Session Manager.");
+		sessionManager = new SessionManagerImpl(context, monitor, publisher);
+		
+		LogUtils.logDebug(context, getClass(), "start", "Starting Susbscriber.");
+		subscriber = new Subscriber(context, sessionManager);
+		
+
+		LogUtils.logDebug(context, getClass(), "start", "Adding services.");
+		scallee = new SCallee(context, SessionManagerService.initialize(context), sessionManager);
+		LogUtils.logDebug(context, getClass(), "start", "Started.");
+	}
+
+
+	public void stop(ModuleContext ctxt) throws Exception {
+		LogUtils.logDebug(context, getClass(), "stop", "Stopping.");
+		/*
+		 * close uAAL stuff
+		 */
+		if (publisher != null){
+		    publisher.close();
+			LogUtils.logDebug(context, getClass(), "start", "Stopping.");
+		    publisher = null;
+		}
+		if (monitor != null){
+		    monitor.close();
+		    monitor = null;
+		}
+		if (subscriber != null) {
+		    subscriber.close();
+		    subscriber = null;
+		}
+		if (scallee != null) {
+		    scallee.close();
+		    scallee = null;
+		}
+		
+		LogUtils.logDebug(context, getClass(), "stop", "Stopped.");
+
+	}
+
+}
---
+++ security.session.manager/src/test/java/deprecated/LocationTreeNode.java
@@ -0,0 +1,125 @@
+/*******************************************************************************
+ * Copyright 2013 Universidad Politécnica de Madrid
+ * Copyright 2013 Fraunhofer-Gesellschaft - Institute for Computer Graphics Research
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ******************************************************************************/
+
+package deprecated;
+
+import java.util.Iterator;
+import java.util.List;
+import java.util.Set;
+
+import org.universAAL.ontology.location.Location;
+
+/**
+ * @author amedrano
+ *
+ */
+public class LocationTreeNode {
+
+    Location loc;
+    
+    Set<LocationTreeNode> children;
+
+    private LocationTreeNode parent;
+    
+    /**
+     * 
+     */
+    public LocationTreeNode(Location l) {
+	loc = l;
+	Object contains = loc.getProperty(Location.PROP_CONTAINS);
+	if (contains instanceof Location){
+	    add((Location) contains);
+	}
+	if (contains instanceof List<?>){
+	    for (Object con : (List)contains) {
+		if (con instanceof Location){
+		    add((Location) con);
+		}
+	    }
+	}
+    }
+        
+    public void add(Location l){
+	if (l != loc && l != null){
+	    LocationTreeNode nLTN = new LocationTreeNode(l);
+	    LocationTreeNode parent = search(nLTN.getLocationParent());
+	    if (parent != null){
+		nLTN.parent = parent;
+		parent.children.add(nLTN);
+	    }else {
+		nLTN.parent = this;
+		children.add(nLTN);
+	    }
+	}
+    }
+    
+    public void add(LocationTreeNode ltn){
+	if (ltn != null && ltn.loc != loc) {
+	   LocationTreeNode addRoot = search(ltn.loc);
+	   if (addRoot != null){
+	       for (LocationTreeNode cltn : ltn.children) {
+		cltn.parent = addRoot;
+		addRoot.children.add(cltn);
+	    }
+	   }
+	   else {
+	    addRoot = search(ltn.getLocationParent());
+	    if (addRoot != null) {
+		ltn.parent = addRoot;
+		addRoot.children.add(ltn);
+	    }
+	    else {
+		ltn.parent = this;
+		children.add(ltn);
+	    }
+	}
+	}
+    }
+    
+    public LocationTreeNode search(Location l){
+	LocationTreeNode found = null;
+	Iterator<LocationTreeNode> it = children.iterator();
+	while (l != null
+		&& found == null
+		&& it.hasNext()){
+	    found = it.next().search(l);
+	}
+	return found;
+    }
+
+    public LocationTreeNode getParent(){
+	return parent;
+    }
+    
+    public LocationTreeNode getRoot(){
+	if (parent == null){
+	    return this;
+	} else {
+	    return parent.getRoot();
+	}
+    }
+    
+    public Location getLocationParent(){
+	return (Location) loc.getProperty(Location.PROP_IS_CONTAINED_IN);
+    }
+
+    /** {@ inheritDoc}	 */
+    public boolean equals(LocationTreeNode obj) {
+	return loc.equals(obj.loc);
+    }
+
+}
